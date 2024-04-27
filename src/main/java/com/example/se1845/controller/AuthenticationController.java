cc63dd0fd08a7213b2fd6aac59ddf4afe0bb8761
minhsimple <88241158+minhsimple@users.noreply.github.com>
2024-04-27 14:23:26 +0700

    commit add AuthenticationManagerException

--- src/main/java/com/example/se1845/controller/AuthenticationController.java
+++ src/main/java/com/example/se1845/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,12 +31,17 @@ public class AuthenticationController {
 
     @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("AuthenticationManager authenticate error", HttpStatus.UNAUTHORIZED);
+        }
+
         final UserDetails user = userDao.findUserByEmail(request.getEmail());
         if (user != null) {
             return new ResponseEntity<>(jwtUtil.generateToken(user), HttpStatus.OK);
         }
-        return new ResponseEntity<>("error", HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>("error user not found", HttpStatus.UNAUTHORIZED);
     }
 }
