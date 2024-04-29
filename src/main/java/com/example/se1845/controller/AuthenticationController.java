5cec3a0ef54f2ea9cada03d342839dcaad048880
minhsimple <88241158+minhsimple@users.noreply.github.com>
2024-04-29 19:39:05 +0700

    commit add validation

--- src/main/java/com/example/se1845/controller/AuthenticationController.java
+++ src/main/java/com/example/se1845/controller/AuthenticationController.java
@@ -1,5 +1,8 @@
 package com.example.se1845.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,14 +10,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.se1845.Config.JwtUtil;
 import com.example.se1845.dto.AuthenticationRequest;
 import com.example.se1845.service.EmployeeService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
@@ -30,7 +39,7 @@ public class AuthenticationController {
     private JwtUtil jwtUtil;
 
     @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<String> authenticate(@Valid @RequestBody AuthenticationRequest request) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
@@ -40,5 +49,17 @@ public class AuthenticationController {
 
         final UserDetails user = employeeService.findEmployeeByEmail(request.getEmail()).get();
         return new ResponseEntity<>(jwtUtil.generateToken(user), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- src/main/java/com/example/se1845/dto/AuthenticationRequest.java
+++ src/main/java/com/example/se1845/dto/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package com.example.se1845.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
 
+    @NotNull(message = "email cannot be null")
+    @NotBlank(message = "email cannot be blank")
     private String email;
+
+    @NotNull(message = "password cannot be null")
+    @NotBlank(message = "password cannot be blank")
     private String password;
 }
