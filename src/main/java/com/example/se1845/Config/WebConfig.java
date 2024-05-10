f857fcacb43974d5b792d51166008b48a7ff7f11
minhsimple <88241158+minhsimple@users.noreply.github.com>
2024-05-11 00:42:17 +0700

    commit add WebConfig

---
+++ src/main/java/com/example/se1845/Config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.se1845.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
