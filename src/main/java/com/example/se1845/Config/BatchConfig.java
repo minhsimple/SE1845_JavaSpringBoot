723fc998d66ccd683b85dafaa7915246f973b097
minhsimple <88241158+minhsimple@users.noreply.github.com>
2024-05-12 17:06:50 +0700

    commit fix sending mail performance

--- src/main/java/com/example/se1845/Config/BatchConfig.java
+++ src/main/java/com/example/se1845/Config/BatchConfig.java
@@ -4,6 +4,11 @@ import javax.sql.DataSource;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,7 +17,6 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
@@ -26,8 +30,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.se1845.dto.MailBody;
 import com.example.se1845.dto.NotificationDTO;
-import com.example.se1845.model.Employee;
-import com.example.se1845.repository.EmployeeRepository;
 import com.example.se1845.service.EmailService;
 
 @Configuration
@@ -46,11 +48,28 @@ public class BatchConfig {
     @Autowired
     private JobRepository jobRepository;
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private List<String> emails;
 
     @Bean
-    public JdbcCursorItemReader<NotificationDTO> reader() {
+    public JdbcCursorItemReader<String> emailAccountReader() {
+        return new JdbcCursorItemReaderBuilder<String>()
+                .dataSource(dataSource)
+                .name("emailAccountReader")
+                .sql("select Email from Employee")
+                .rowMapper(new RowMapper<String>() {
+                    @Override
+                    @Nullable
+                    @SuppressWarnings("null")
+                    public String mapRow(ResultSet rs, int rowNum)
+                            throws SQLException {
+                        return rs.getString("Email");
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<NotificationDTO> notificationReader() {
         return new JdbcCursorItemReaderBuilder<NotificationDTO>()
                 .dataSource(dataSource)
                 .name("notificationItemReader")
@@ -75,36 +94,54 @@ public class BatchConfig {
     }
 
     @Bean
-    public ItemWriter<NotificationDTO> writer() {
+    public ItemWriter<String> emailAccountWriter() {
+        return items -> emails = new ArrayList<>(items.getItems());
+    }
+
+    @Bean
+    public ItemWriter<NotificationDTO> sendEmail() {
         return items -> {
-            Iterable<Employee> employees = employeeRepository.findAll();
-            for (Employee employee : employees) {
-                for (NotificationDTO item : items) {
-                    MailBody mailBody = MailBody.builder()
-                            .to(employee.getEmail())
-                            .subject("Notification: " + item.getTitle())
-                            .text(item.getDetail())
-                            .build();
+            ExecutorService firstExecutor = Executors.newFixedThreadPool(10);
+            for (String email : emails) {
+                firstExecutor.submit(() -> {
+                    for (NotificationDTO item : items) {
+                        MailBody mailBody = MailBody.builder()
+                                .to(email)
+                                .subject("Notification: " + item.getTitle())
+                                .text(item.getDetail())
+                                .build();
 
-                    emailService.sendSimpleMessage(mailBody);
-                }
+                        emailService.sendSimpleMessage(mailBody);
+                    }
+                });
             }
-
+            firstExecutor.shutdown();
+            try {
+                // Wait until all tasks are finished
+                firstExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         };
     }
 
     @Bean
-    public ItemProcessor<NotificationDTO, NotificationDTO> processor() {
-        return new PassThroughItemProcessor<>();
+    public Step readEmailAccountStep() {
+        return new StepBuilder("readEmailAccountStep", jobRepository)
+                .<String, String>chunk(10, platformTransactionManager)
+                .reader(emailAccountReader())
+                .processor(new PassThroughItemProcessor<>())
+                .writer(emailAccountWriter())
+                .build();
     }
 
     @Bean
-    public Step executeStep() {
-        return new StepBuilder("executeStep", jobRepository)
+    public Step sendEmailStep() {
+        return new StepBuilder("sendEmailStep", jobRepository)
                 .<NotificationDTO, NotificationDTO>chunk(10, platformTransactionManager)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
+                .reader(notificationReader())
+                .processor(new PassThroughItemProcessor<>())
+                .writer(sendEmail())
                 .build();
     }
 
@@ -112,7 +149,8 @@ public class BatchConfig {
     public Job processJob() {
         return new JobBuilder("processJob", jobRepository)
                 .incrementer(new RunIdIncrementer())
-                .start(executeStep())
+                .start(readEmailAccountStep())
+                .next(sendEmailStep())
                 .build();
     }
 }
